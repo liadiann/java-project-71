5d2b35484fa8e753c2cc4ed6203a787c34d4e0af
Julia Kon <kon1976y@gmail.com>
2025-03-08 23:11:38 +0300

    add gradle project "app"

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.12.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
