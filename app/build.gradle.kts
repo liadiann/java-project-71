1dfc2a59fb724a44773e88b8ff8c40155b166ccf
Julia Kon <kon1976y@gmail.com>
2025-03-08 23:39:31 +0300

    add application plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    application
 }
 
 group = "hexlet.code"
@@ -14,6 +15,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.12.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
