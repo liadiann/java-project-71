f290ff71d58b998f739f02d0745a904c85e8a5b1
Julia Kon <kon1976y@gmail.com>
2025-03-09 17:54:47 +0300

    add options and parameters picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,16 @@ import picocli.CommandLine.Parameters;
 	description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
+	@Option(names = {"-f", "--format"}, paramLabel = "format",
+		description = "output format [default: stylish]", defaultValue = "stylish")
+	private String format = "stylish";
+
+	@Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+	private String filepath1;
+
+	@Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+	private String filepath2;
+
 	@Override
 	public void run() {
 		System.out.println("Hello");
