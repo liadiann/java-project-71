47be1d30ca9e582cf5b68564ffab41f25b990946
Julia Kon <kon1976y@gmail.com>
2025-03-10 23:21:00 +0300

    fix problems checkstyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,28 +7,28 @@ import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-	description = "Compares two configuration files and shows a difference.")
+    description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
-	@Option(names = {"-f", "--format"}, paramLabel = "format",
-		description = "output format [default: stylish]", defaultValue = "stylish")
-	private String format = "stylish";
+    @Option(names = {"-f", "--format"}, paramLabel = "format",
+            description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format = "stylish";
 
-	@Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
-	private String filepath1;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
 
-	@Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
-	private String filepath2;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
 
-	@Override
-	public Integer call() throws Exception {
-		var str = Differ.generate(filepath1, filepath2);
-		System.out.println(str);
-		return 0;
-	}
+    @Override
+    public Integer call() throws Exception {
+        var str = Differ.generate(filepath1, filepath2);
+        System.out.println(str);
+        return 0;
+    }
 
-	public static void main(String[] args) {
-		int exitCode = new CommandLine(new App()).execute(args);
-		System.exit(exitCode);
-	}
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -11,44 +11,46 @@ import java.util.TreeSet;
 
 public class Differ {
 
-	private static String readFile(String filename) throws Exception {
-		var path = Paths.get("src", "test", "resources", "fixtures", filename).toAbsolutePath().normalize();
-		return Files.readString(path).trim();
-	}
+    private static String readFile(String filename) throws Exception {
+        var path = Paths.get("src", "test", "resources", "fixtures", filename).toAbsolutePath().normalize();
+        return Files.readString(path).trim();
+    }
 
-	private static Map<String, Object> parse(String json) throws Exception {
-		var objectMapper = new ObjectMapper();
-		return objectMapper.readValue(json, new TypeReference<Map<String, Object>>(){});
-	}
+    private static Map<String, Object> parse(String json) throws Exception {
+        var objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, new TypeReference<>() { });
+    }
 
-	public static String generate(String filepath1, String filepath2) throws Exception {
-		var json1 = readFile(filepath1);
-		var mapJson1 = parse(json1);
-		var sortedMap1 = new TreeMap<String, Object>(mapJson1);
-		var json2 = readFile(filepath2);
-		var mapJson2 = parse(json2);
-		var sortedMap2 = new TreeMap<String, Object>(mapJson2);
-		var keys = new TreeSet<String>(sortedMap1.keySet());
-		keys.addAll(sortedMap2.keySet());
-		var result = new StringBuilder("{\n");
-		keys.forEach(key -> {
-			if (!sortedMap2.containsKey(key)) {
-				var tmp = "  - " + key + ": " + sortedMap1.get(key) + "\n";
-				result.append(tmp);
-			} else if (!sortedMap1.containsKey(key)) {
-				var tmp = "  + " + key + ": " + sortedMap2.get(key) + "\n";
-				result.append(tmp);
-			} else if (sortedMap1.get(key).equals(sortedMap2.get(key))) {
-				var tmp = "    " + key + ": " + sortedMap1.get(key) + "\n";
-				result.append(tmp);
-			} else {
-				var tmp = "  - " + key + ": " + sortedMap1.get(key) + "\n";
-				result.append(tmp);
-				tmp = "  + " + key + ": " + sortedMap2.get(key) + "\n";
-				result.append(tmp);
-			}
-		});
-		result.append("}");
-		return result.toString();
-	}
+    public static String generate(String filepath1, String filepath2) throws Exception {
+        var json1 = readFile(filepath1);
+        var mapJson1 = parse(json1);
+        var sortedMap1 = new TreeMap<>(mapJson1);
+
+        var json2 = readFile(filepath2);
+        var mapJson2 = parse(json2);
+        var sortedMap2 = new TreeMap<>(mapJson2);
+
+        var keys = new TreeSet<>(sortedMap1.keySet());
+        keys.addAll(sortedMap2.keySet());
+        var result = new StringBuilder("{\n");
+        keys.forEach(key -> {
+            if (!sortedMap2.containsKey(key)) {
+                var tmp = "  - " + key + ": " + sortedMap1.get(key) + "\n";
+                result.append(tmp);
+            } else if (!sortedMap1.containsKey(key)) {
+                var tmp = "  + " + key + ": " + sortedMap2.get(key) + "\n";
+                result.append(tmp);
+            } else if (sortedMap1.get(key).equals(sortedMap2.get(key))) {
+                var tmp = "    " + key + ": " + sortedMap1.get(key) + "\n";
+                result.append(tmp);
+            } else {
+                var tmp = "  - " + key + ": " + sortedMap1.get(key) + "\n";
+                result.append(tmp);
+                tmp = "  + " + key + ": " + sortedMap2.get(key) + "\n";
+                result.append(tmp);
+            }
+        });
+        result.append("}");
+        return result.toString();
+    }
 }
