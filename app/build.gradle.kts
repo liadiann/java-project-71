5edad911f7565e5357923d045a1e833ee91daa32
Julia Kon <kon1976y@gmail.com>
2025-03-10 23:20:14 +0300

    add checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.52.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     application
+    checkstyle
 }
 
 group = "hexlet.code"
