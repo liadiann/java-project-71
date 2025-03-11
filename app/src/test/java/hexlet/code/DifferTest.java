f008a1d84adf5d61dc432e9b1b351ca2a330f56e
Julia Kon <kon1976y@gmail.com>
2025-03-11 19:31:18 +0300

    add tests for yaml files

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
@@ -9,18 +10,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
+	private static String expected;
+
+	@BeforeAll
+	public static void beforeAll() throws Exception {
+		expected = readFile("result");
+	}
+
     private static String readFile(String filename) throws Exception {
         var path = Paths.get("src", "test", "resources", filename).toAbsolutePath().normalize();
         return Files.readString(path).trim();
     }
 
     @Test
-    public void testCompare() throws Exception {
+    public void testCompareJson() throws Exception {
         var filename1 = "file1.json";
         var filename2 = "file2.json";
         var actual = Differ.generate(filename1, filename2);
-        var expected = readFile("result");
         assertEquals(expected, actual);
     }
 
+	@Test
+	public void testCompareYml() throws Exception {
+		var filename1 = "file1.yaml";
+		var filename2 = "file2.yaml";
+		var actual = Differ.generate(filename1, filename2);
+		assertEquals(expected, actual);
+	}
 }
