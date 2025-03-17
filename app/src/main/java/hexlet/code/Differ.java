8ca0dd8b60c3d1cebc280d47adf1564fd81af5ba
Julia Kon <kon1976y@gmail.com>
2025-03-17 17:55:46 +0300

    fix Parser.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,13 +1,26 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Differ {
+
+    private static String readFile(String filename) throws Exception {
+        if (Paths.get(filename).isAbsolute()) {
+            return Files.readString(Paths.get(filename).normalize()).trim();
+        }
+        var path = Paths.get("src", "test", "resources", "fixtures", filename).toAbsolutePath().normalize();
+        return Files.readString(path).trim();
+    }
 
     public static String generate(String filepath1, String filepath2, String formatter) throws Exception {
         var format = filepath1.split("\\.")[1];
-        var map1 = Parser.parse(filepath1, format);
-        var map2 = Parser.parse(filepath2, format);
-        var sortedMap = FormationDiff.calculateDiff(map1, map2);
-        return Formatter.formatIt(formatter, sortedMap);
+        var fileContent1 = readFile(filepath1);
+        var fileContent2 = readFile(filepath2);
+        var parsedFileContest1 = Parser.parse(fileContent1, format);
+        var parsedFileContest2 = Parser.parse(fileContent2, format);
+        var diff = FormationDiff.calculate(parsedFileContest1, parsedFileContest2);
+        return Formatter.formatIt(formatter, diff);
     }
 
     public static String generate(String filepath1, String filepath2) throws Exception {
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,28 +4,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
 
-    private static String readFile(String filename) throws Exception {
-        if (Paths.get(filename).isAbsolute()) {
-            return Files.readString(Paths.get(filename).normalize()).trim();
-        }
-        var path = Paths.get("src", "test", "resources", "fixtures", filename).toAbsolutePath().normalize();
-        return Files.readString(path).trim();
-    }
-
-    public static Map<String, Object> parse(String filename, String format) throws Exception {
+    public static Map<String, Object> parse(String fileContent, String format) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         if (format.equals("yaml") || format.equals("yml")) {
             objectMapper = new YAMLMapper();
         }
-        var str = readFile(filename);
-        return objectMapper.readValue(str, new TypeReference<>() {
+        return objectMapper.readValue(fileContent, new TypeReference<>() {
         });
     }
 }
